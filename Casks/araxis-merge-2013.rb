b7d0b7098ddce70b7c43a4bc2e9b98ddbe6a390b
makoto_kw <makoto.kw@gmail.com>
2016-05-09 13:44:21 +0900

    Add Araxis Merge.app 2013.4415

---
+++ Casks/araxis-merge-2013.rb
@@ -0,0 +1,46 @@
+cask 'araxis-merge-2013' do
+  version '2013.4415'
+
+  if MacOS.release <= :snow_leopard
+    sha256 :no_check
+    url "http://www.araxis.com/download/Merge#{version}-OSX10.6.dmg"
+  elsif MacOS.release <= :lion
+    sha256 :no_check
+    url "http://www.araxis.com/download/Merge#{version}-OSX10.8.dmg"
+  elsif MacOS.release <= :mountain_lion
+    sha256 :no_check
+    url "http://www.araxis.com/download/Merge#{version}-OSX10.8.dmg"
+  else
+    sha256 '6fdc0008ec06c51c3a4d41d6458aaca627f6746a1c876fc6c8b79eaf3e19721f'
+    url "http://www.araxis.com/download/Merge#{version}-OSX10.9.dmg"
+  end
+
+  name 'Araxis Merge 2013'
+  homepage 'http://www.araxis.com/merge'
+  license :commercial
+
+  depends_on macos: '>= :snow_leopard'
+
+  app 'Araxis Merge.app'
+  binary 'Utilities/araxisgitdiff'
+  binary 'Utilities/araxisopendiff'
+  binary 'Utilities/araxissvndiff'
+  binary 'Utilities/compare'
+  binary 'Utilities/araxisgitmerge'
+  binary 'Utilities/araxisp4diff'
+  binary 'Utilities/araxissvndiff3'
+  binary 'Utilities/compare2'
+  binary 'Utilities/araxishgmerge'
+  binary 'Utilities/araxisp4winmrg'
+  binary 'Utilities/araxissvnmerge'
+
+  zap delete: [
+                '~/Library/Preferences/com.araxis.merge.LSSharedFileList.plist',
+                '~/Library/Preferences/com.araxis.merge.plist',
+              ]
+
+  caveats <<-EOS.undent
+    For instructions to integrate Araxis Merge with Finder or other applications,
+    see http://www.araxis.com/merge/documentation-os-x/installing.en
+  EOS
+end
